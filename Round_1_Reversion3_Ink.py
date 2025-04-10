a1f761fa7b3a6f1f00640bd5ea58a2e3679ed5f9
Neal Pannala <nealpannala@Neals-MacBook-Air.local>
2025-04-09 23:10:31 -0500

    final rd 1 edits

--- Round_1_Reversion3_Ink.py
+++ Round_1_Reversion3_Ink.py
@@ -234,16 +234,123 @@ class KelpStrategy(MarketMakingStrategy):
 
         return round((popular_buy_price + popular_sell_price) / 2)
 
+# class Squid_InkStrategy(MarketMakingStrategy):
+#     def get_true_value(self, state: TradingState) -> int:
+#         order_depth = state.order_depths[self.symbol]
+
+#         order_depth = state.order_depths[self.symbol]
+#         buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+#         sell_orders = sorted(order_depth.sell_orders.items())
+
+#         popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+#         popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+
+#         return round((popular_buy_price + popular_sell_price) / 2)
+
+class InkStrategy(Strategy):
+    def __init__(self, symbol: Symbol, limit: int) -> None:
+        super().__init__(symbol, limit)
+
+        self.window = deque()
+        self.window_size = 10
+
+    def get_true_value(self, state: TradingState) -> int:
+        order_depth = state.order_depths[self.symbol]
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+        popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+
+        return round((popular_buy_price + popular_sell_price) / 2)
+
+    def act(self, state: TradingState) -> None:
+        true_value = self.get_true_value(state)
+
+        order_depth = state.order_depths[self.symbol]
+        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
+        sell_orders = sorted(order_depth.sell_orders.items())
+
+        position = state.position.get(self.symbol, 0)
+        to_buy = self.limit - position
+        to_sell = self.limit + position
+        if true_value > 1950:
+            to_buy = 0
+        if true_value < 2050:
+            to_sell = 0
+
+        self.window.append(abs(position) == self.limit)
+        if len(self.window) > self.window_size:
+            self.window.popleft()
+
+        soft_liquidate = len(self.window) == self.window_size and sum(self.window) >= self.window_size / 2 and self.window[-1]
+        hard_liquidate = len(self.window) == self.window_size and all(self.window)
+
+        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
+        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
+
+        for price, volume in sell_orders:
+            if to_buy > 0 and price <= max_buy_price:
+                quantity = min(to_buy, -volume)
+                self.buy(price, quantity)
+                to_buy -= quantity
+
+        if to_buy > 0 and hard_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0 and soft_liquidate:
+            quantity = to_buy // 2
+            self.buy(true_value - 2, quantity)
+            to_buy -= quantity
+
+        if to_buy > 0:
+            popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
+            price = min(max_buy_price, popular_buy_price + 1)
+            self.buy(price, to_buy)
+
+        for price, volume in buy_orders:
+            if to_sell > 0 and price >= min_sell_price:
+                quantity = min(to_sell, volume)
+                self.sell(price, quantity)
+                to_sell -= quantity
+
+        if to_sell > 0 and hard_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0 and soft_liquidate:
+            quantity = to_sell // 2
+            self.sell(true_value + 2, quantity)
+            to_sell -= quantity
+
+        if to_sell > 0:
+            popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
+            price = max(min_sell_price, popular_sell_price - 1)
+            self.sell(price, to_sell)
+
+    def save(self) -> JSON:
+        return list(self.window)
+
+    def load(self, data: JSON) -> None:
+        self.window = deque(data)
+
 class Trader:
     def __init__(self) -> None:
         limits = {
-            "RAINFOREST_RESIN": 20,
-            "KELP": 20,
+            "RAINFOREST_RESIN": 50,
+            "KELP": 50,
+            "SQUID_INK" : 50
         }
 
         self.strategies = {symbol: clazz(symbol, limits[symbol]) for symbol, clazz in {
             "RAINFOREST_RESIN": Rainforest_ResinStrategy,
             "KELP": KelpStrategy,
+            "SQUID_INK": InkStrategy
         }.items()}
 
     def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
@@ -256,13 +363,15 @@ class Trader:
 
         orders = {}
         for symbol, strategy in self.strategies.items():
-            if symbol in old_trader_data:
-                strategy.load(old_trader_data.get(symbol, None))
+            # mid_price = strategy.get_true_value(state)
+            # if symbol != "SQUID_INK" or (1950 > mid_price or 2050 < mid_price):
+                if symbol in old_trader_data:
+                    strategy.load(old_trader_data.get(symbol, None))
 
-            if symbol in state.order_depths:
-                orders[symbol] = strategy.run(state)
+                if symbol in state.order_depths:
+                    orders[symbol] = strategy.run(state)
 
-            new_trader_data[symbol] = strategy.save()
+                new_trader_data[symbol] = strategy.save()
 
         trader_data = json.dumps(new_trader_data, separators=(",", ":"))
 
