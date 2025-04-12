7e7ab43eecfbeed45740fa48db08c9132455b8ff
Ashwin Balaraman <abalaraman1@uchicago.edu>
2025-04-12 18:38:50 -0500

    Refactor code structure for improved readability and maintainability

--- Rount_2.py
+++ Rount_2.py
@@ -234,19 +234,6 @@ class KelpStrategy(MarketMakingStrategy):
 
         return round((popular_buy_price + popular_sell_price) / 2)
 
-# class Squid_InkStrategy(MarketMakingStrategy):
-#     def get_true_value(self, state: TradingState) -> int:
-#         order_depth = state.order_depths[self.symbol]
-
-#         order_depth = state.order_depths[self.symbol]
-#         buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
-#         sell_orders = sorted(order_depth.sell_orders.items())
-
-#         popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
-#         popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
-
-#         return round((popular_buy_price + popular_sell_price) / 2)
-
 class InkStrategy(Strategy):
     def __init__(self, symbol: Symbol, limit: int) -> None:
         super().__init__(symbol, limit)
@@ -269,69 +256,21 @@ class InkStrategy(Strategy):
     def act(self, state: TradingState) -> None:
         true_value = self.get_true_value(state)
 
-        order_depth = state.order_depths[self.symbol]
-        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
-        sell_orders = sorted(order_depth.sell_orders.items())
-
         position = state.position.get(self.symbol, 0)
-        to_buy = self.limit - position
-        to_sell = self.limit + position
-        if true_value > 1950:
-            to_buy = 0 #we got to sell all long positions
-        if true_value < 2050:
-            to_sell = 0 #Convert all short positions
 
-        self.window.append(abs(position) == self.limit)
-        if len(self.window) > self.window_size:
-            self.window.popleft()
-
-        soft_liquidate = len(self.window) == self.window_size and sum(self.window) >= self.window_size / 2 and self.window[-1]
-        hard_liquidate = len(self.window) == self.window_size and all(self.window)
-
-        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
-        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
-
-        for price, volume in sell_orders:
-            if to_buy > 0 and price <= max_buy_price:
-                quantity = min(to_buy, -volume)
-                self.buy(price, quantity)
-                to_buy -= quantity
-
-        if to_buy > 0 and hard_liquidate:
-            quantity = to_buy // 2
-            self.buy(true_value, quantity)
-            to_buy -= quantity
-
-        if to_buy > 0 and soft_liquidate:
-            quantity = to_buy // 2
-            self.buy(true_value - 2, quantity)
-            to_buy -= quantity
-
-        if to_buy > 0:
-            popular_buy_price = max(buy_orders, key=lambda tup: tup[1])[0]
-            price = min(max_buy_price, popular_buy_price + 1)
-            self.buy(price, to_buy)
-
-        for price, volume in buy_orders:
-            if to_sell > 0 and price >= min_sell_price:
-                quantity = min(to_sell, volume)
-                self.sell(price, quantity)
-                to_sell -= quantity
-
-        if to_sell > 0 and hard_liquidate:
-            quantity = to_sell // 2
-            self.sell(true_value, quantity)
-            to_sell -= quantity
-
-        if to_sell > 0 and soft_liquidate:
-            quantity = to_sell // 2
-            self.sell(true_value + 2, quantity)
-            to_sell -= quantity
-
-        if to_sell > 0:
-            popular_sell_price = min(sell_orders, key=lambda tup: tup[1])[0]
-            price = max(min_sell_price, popular_sell_price - 1)
-            self.sell(price, to_sell)
+        ub = 2050
+        lb = 1950
+        if ub > true_value > lb:
+            if position > 0:
+                self.sell(true_value, position)
+            elif position < 0:
+                self.buy(true_value, -position)
+        elif true_value >= ub:
+            to_sell = min(self.limit + position, self.limit)
+            self.sell(true_value, to_sell)
+        elif true_value <= lb:
+            to_buy = min(self.limit - position, self.limit)
+            self.buy(true_value, to_buy)
 
     def save(self) -> JSON:
         return list(self.window)
